639168b75088b49952f37ee9497aa7cd56dd2d06
yuki-kaneko-git <yuki.kaneko.git@gmail.com>
2022-08-16 11:24:52 +0900

    HTML5のバリデーションに戻しました

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -5,83 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>社員登録画面</title>
-    <script type="text/javascript">
-        function check(){
-            if(check.id.value == ""){
-                alert("社員IDを入力してください");
-                return false;
-            }else if(!check.id.value.match({10})){
-                alert("社員IDは10文字で入力してください");
-                return false;
-            }else if(!check.id.value.match(^YZ\d$)){
-                alert("社員IDを正しく入力してください");
-                return false;
-            }else if(check.lastNm.value == ""){
-                alert("社員名（姓）を入力してください");
-                return false;
-            }else if(!check.lastNm.value.match({, 25})){
-                alert("社員名（姓）は25文字以内で入力してください");
-                return false;
-            }else if(check.firstNm.value == ""){
-                alert("社員名（名）を入力してください");
-                return false;
-            }else if(!check.firstNm.value.match({, 25})){
-                alert("社員名（名）は25文字以内で入力してください");
-                return false;
-            }else if(check.section.value == ""){
-                alert("所属セクションを入力してください");
-                return false;
-            }else if(check.mail.value == ""){
-                alert("メールアドレスを入力してください");
-                return false;
-            }else if(!check.mail.value.match({, 256})){
-                alert("メールアドレスは256文字以内で入力してください");
-                return false;
-            }else if(!check.mail.value.match(^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+$)){
-                alert("メールアドレスを正しく入力してください");
-                return false;
-            }else if(check.gender.value == ""){
-                alert("性別を入力してください");
-                return false;
-            }else{
-                return true;
-            }
-        }
-    </script>
 </head>
 <body>
     <div class="insert">
-        <form method="" action="" name="check">
+        <form method="" action="">
             <table>
-                <tr><th>社員ID*</th><td><input type="text" placeholder="YZ12345678" name="id"></td></tr>
+                <tr><th>社員ID*</th><td><input type="text" placeholder="YZ12345678" pattern="^YZ\d{8}$" required title=""></td></tr>
                 <tr><th>社員名*</th>
                     <td>
-                        <input type="text" placeholder="姓" name="lastNm">
+                        <input type="text" placeholder="姓" maxlength="25" required>
                     </td>
                     <td>
-                        <input type="text" placeholder="名" name="firstNm">
+                        <input type="text" placeholder="名" maxlength="25" required>
                     </td>
                 </tr>
                 <tr><th>所属セクション*</th>
                     <td>
-                        <select name="section" name="section">
+                        <select name="section" required>
                             <option hidden>選択してください</option>
                             <option value="シス開">シス開</option>
                             <option value="グロカル">グロカル</option>
                             <option value="ビジソル">ビジソル</option>
                         </select> 
                     </td></tr>
-                <tr><th>メールアドレス*</th><td><input type="text" placeholder="taro_yaz@yaz.co.jp" name="mail"></td></tr>
+                <tr><th>メールアドレス*</th><td><input type="text" placeholder="taro_yaz@yaz.co.jp" pattern="^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+$" maxlength="256" required></td></tr>
                 <tr><th>性別*</th>
                     <td>
-                        <label><input type="radio" name="gender">男性</label>
-                        <label><input type="radio" name="gender">女性</label>
+                        <label><input type="radio" name="gender" required>男性</label>
+                        <label><input type="radio" name="gender" required>女性</label>
                     </td>
                 </tr>
             </table>
             <p>*必須項目</p>
             <p></p>
-            <input type="submit" value="登録" onclick="return check()">
+            <input type="submit" value="登録">
             <p></p>
             <a href="{{route('menu')}}">メニュー画面</a>
         </form>
